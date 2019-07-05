4e3695d85bd2de9c05db2df6faf02dc8a0efa256
nenad1995 <nenad.zoric@yahoo.com>
2019-07-05 18:36:47 +0200

    Zadatak 2

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    Placeholder
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <div>{{ post.text }}</div>
+      <div>Posted at : {{ post.createdAt }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import { posts } from '../services/post-service'
+
 export default {
- 
+  data () {
+    return {
+      posts: []
+    }
+  },
+
+  beforeRouteEnter (to, from, next) {
+    posts.getAll()
+      .then((response) => {
+        next((vm) => {
+          vm.posts = response.data
+        })
+      })
+  }
 }
 </script>
